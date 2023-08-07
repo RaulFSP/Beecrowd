9811bf04a5b5cd476e284de2b0fb887ddb1e0043
RaulFSP <135935707+RaulFSP@users.noreply.github.com>
2023-08-06 21:40:13 -0400

    Create 1007_Diferenca.php

---
+++ php 8.1/1007_Diferenca.php
@@ -0,0 +1,8 @@
+<?php
+    $varA = fgets(STDIN);
+    $varB = fgets(STDIN);
+    $varC = fgets(STDIN);
+    $varD = fgets(STDIN);
+    $resultado = ($varA*$varB-$varC*$varD);
+    echo "DIFERENCA = ".$resultado.PHP_EOL;   
+?>
