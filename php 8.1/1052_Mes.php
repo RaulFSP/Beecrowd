493cc7b36ea328945bc20e6f9e9d75f2d2da3510
RaulFSP <135935707+RaulFSP@users.noreply.github.com>
2023-08-17 13:09:31 -0400

    Create 1052_Mes.php

---
+++ php 8.1/1052_Mes.php
@@ -0,0 +1,17 @@
+<?php
+$entrada = trim(fgets(STDIN));
+switch($entrada) {
+    case 1: echo "January".PHP_EOL;break;
+    case 2: echo "February".PHP_EOL;break;
+    case 3: echo "March".PHP_EOL;break;
+    case 4: echo "April".PHP_EOL;break;
+    case 5: echo "May".PHP_EOL;break;
+    case 6: echo "June".PHP_EOL;break;
+    case 7: echo "July".PHP_EOL;break;
+    case 8: echo "August".PHP_EOL;break;
+    case 9: echo "September".PHP_EOL;break;
+    case 10: echo "October".PHP_EOL;break;
+    case 11: echo "November".PHP_EOL;break;
+    case 12: echo "December".PHP_EOL;break;
+}
+?>
