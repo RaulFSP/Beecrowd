7231fe623fba63d322fc8553621120e62d32d426
RaulFSP <135935707+RaulFSP@users.noreply.github.com>
2023-09-20 09:21:01 -0400

    Create 1003.java

---
+++ java 19/1003.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = 0;
+        x += sc.nextInt();
+        x += sc.nextInt();
+        sc.nextLine();        
+        System.out.printf("SOMA = %d\n",x);
+        sc.close();
+    }
+}
